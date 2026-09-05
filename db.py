c1287814c9ae11fb8be44cf0c3651a15b244bc62
qAlcazer <qalcazar99@gmail.com>
2026-09-05 14:39:19 +0300

    feat: update jop status

--- db.py
+++ db.py
@@ -3,12 +3,19 @@ import sqlite3
 connection = sqlite3.connect("wadhefa.db")
 cursor = connection.cursor()
 
-cursor.execute("SELECT id, title, company, status FROM jobs")
+cursor.execute("UPDATE jobs SET status = ? WHERE id = ?",("applied", 1))
+
+if cursor.rowcount == 0:
+    print("No rows were updated.")
+else:
+    print(f"{cursor.rowcount} row(s) were updated.")
+
+connection.commit()
+
+cursor.execute("SELECT id , title, status FROM jobs")
 rows = cursor.fetchall()
-print(f"total job {len(rows)}")   
+
 for row in rows:
-    print(f"[{row[0]}] {row[1]} — {row[2]} {row[3]}")
-
-
+    print(f"ID: [{row[0]}]   Title: {row[1]}  Status: {row[2]}")
 connection.close()
    
